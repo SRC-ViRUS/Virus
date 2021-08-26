4c63e56d1dbc04bf1e54802528161f24205c216e
SRC-ViRUS <89497238+SRC-ViRUS@users.noreply.github.com>
2021-08-26 04:27:48 +0300

    Update Virus.lua

--- Virus.lua
+++ Virus.lua
@@ -11534,7 +11534,7 @@ keyboard.inline_keyboard = {
 {{text = '☭ مبرمج السورس',url="t.me/OOO0OOQ"}},
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/FFFGF&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/OOO0OOQ&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 return false
 end
 --     Source Virus     --
